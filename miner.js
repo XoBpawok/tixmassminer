329d912a89c811795654d37e8fd9e510133c7b34
mkordiak <mkordiak@cranems.com>
2016-02-10 09:56:24 +0200

    removed random time log. changed mining timers

--- miner.js
+++ miner.js
@@ -2,16 +2,17 @@ var Connection = require('./Connection.js'),
 	fs = require('fs');
 
 loopFunc();
-setInterval(loopFunc, 47 * 60 * 1000);
+setInterval(loopFunc, 27 * 60 * 1000);
 
 function loopFunc() {
+	console.log('/---------------------------------');
+	console.log('Queue started at ' + new Date());
 	fs.readFile('options.json', 'utf8', function(err, data) {
 		var opts = JSON.parse(data);
 		for (var i = opts.credentials.length - 1; i >= 0; i--) {
 			(function(i){
 				var connection = new Connection(opts.credentials[i]);
-				var rand = Math.random() * (17 * 60 * 1000 - 2500) + 2500;
-				console.log(rand);
+				var rand = Math.random() * (26 * 60 * 1000 - 2500) + 2500;
 				setTimeout(function(connection, opts) {
 					connection.setKarmaOpts(opts.karma).start();
 				}, rand, connection, opts);
